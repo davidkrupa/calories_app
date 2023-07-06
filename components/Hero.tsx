9850d998b2586f2a82b5d89fad19ac4b7709885c
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2023-07-06 22:53:37 +0200

    basic structure

---
+++ components/Hero.tsx
@@ -0,0 +1,13 @@
+const Hero = () => {
+  return (
+    <div className="flex xl:flex-row flex-col">
+      <div className="flex-1">
+        <h1 className="text-6xl">Count, Plan, Stay Fit</h1>
+        <p>Keep track of your calories in just 5 minutes a day</p>
+        <button>Start now</button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ components/index.ts
@@ -0,0 +1,5 @@
+import Hero from "./Hero";
+import Activities from "./Activities";
+import Footer from "./Footer";
+
+export { Hero, Activities, Footer };
